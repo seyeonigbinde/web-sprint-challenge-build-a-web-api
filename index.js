fd6ff2544ea4e9a41bd09889982cc56fbdc2f9b3
Your Name <you@example.com>
2021-06-04 16:08:03 +0100

    heroku deployment in index.js

--- index.js
+++ index.js
@@ -13,9 +13,36 @@ I need this code, but don't know where, perhaps should make some middleware, don
 Pull your server into this file and start it!
 */
 
-const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
-});
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
 
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+console.log(process.env.USER) 
+console.log(process.env.SHELL)
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('this means this code is deployed')
+}
+
+const PORT = process.env.PORT || 4000
+
+console.log('port is -> ', PORT)
+
+server.get('/api/projects', (req, res) => {
+  res.json({ message: `${process.env.COHORT} ROCKS` })
+})
+
+server.use((req, res) => {
+  res.status(404).json({ message: 'not found sorry' })
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
